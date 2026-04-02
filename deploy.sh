635dcf9a1d62c37ff11608bdfb08be90f4ff2c73
Earl Sepida <earlsepida63@gmail.com>
2026-04-02 23:07:00 +0800

    Test push

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ~/domains/lumichat.site/public_html/staging || exit
+
+git fetch
+
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin/staging)
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+    echo "New update detected. Deploying..."
+
+    git pull origin staging
+
+    npm install
+    npm run build
+
+    php artisan migrate
+    php artisan optimize:clear
+
+    echo "Deploy completed."
+else
+    echo "No changes."
+fi
+
+
